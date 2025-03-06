e8eac391ef65f26ac535a1530c926aae829ed4b9
alexzuo1 <zuo_al@student.kings.edu.au>
2025-03-06 14:56:03 +1100

    completed player.py

--- player.py
+++ player.py
@@ -3,13 +3,8 @@ import app  # Contains global settings like WIDTH, HEIGHT, PLAYER_SPEED, etc.
 
 class Player:
     def __init__(self, x, y, assets):
-        """Initialize the player with position and image assets."""
-        # TODO: 1. Store the player's position
-        # e.g. self.x = x, self.y = y
-
-        # TODO: 2. Load the player's image from assets
-        # For example: self.image = assets["player_idle"][0]
-        # (or some default image key in assets)
+        self.x = x
+        self.y = y
 
         self.speed = app.PLAYER_SPEED
         self.animations = assets["player"]
@@ -18,31 +13,42 @@ class Player:
         self.animation_timer = 0
         self.animation_speed = 8
 
-        # TODO: 3. Create a collision rectangle (self.rect) 
-        # For example: 
-        # self.rect = self.image.get_rect(center=(self.x, self.y))
-
-        # TODO: 4. Add player health 
-
+        self.image = self.animations[self.state][self.frame_index]
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.facing_left = False
+    
     def handle_input(self):
-        """Check and respond to keyboard/mouse input."""
-
         # TODO: 1. Capture Keyboard Input
-
-        # velocity in X, Y direction
-        vel_x, vel_y = 0, 0
+        keys = pygame.key.get_pressed()
+        
+        vel_x, vel_y = 0, 0 # velocity in X, Y direction
 
         # TODO: 2. Adjust player position with keys pressed, updating the player position to vel_x and vel_y
-
-        # TODO: 3. Clamp player position to screen bounds
-
-        # animation state
+        if key[pygame.K_LEFT]:      #Clamp player position to screen bounds
+            # Move character left
+            vel_x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            vel_x += self.speed
+        if keys[pygame.K_UP]:
+            vel_y -= self.speed
+        if keys[pygame.K_DOWN]:
+            vel_y += self.speed
+        
+        self.x += vel_y
+        self.y += vel_y
+        
+        #player position bound to scr4een 
+        self.x = max(0, min(self.x, app.WIDTH))
+        self.y = max(0, min(self.y, app.HEIGHT))
+        self.rect.center = (self.x, self.y)
+        
+        # determine animation state
         if vel_x != 0 or vel_y != 0:
             self.state = "run"
         else:
             self.state = "idle"
 
-        # direction
+        # facing direction
         if vel_x < 0:
             self.facing_left = True
         elif vel_x > 0:
@@ -62,9 +68,11 @@ class Player:
         pass
 
     def draw(self, surface):
-        """Draw the player on the screen."""
-        # TODO: Draw the image to the given surface at self.rect
-        # For example: surface.blit(self.image, self.rect)
+        if self.facing_left:
+        flipped_img = pygame.transform.flip(self.image, True, False)
+        surface.blit(flipped_img, self.rect)
+        else:
+        surface.blit(self.image, self.rect)
         pass
 
     def take_damage(self, amount):
